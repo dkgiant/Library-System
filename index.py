6d7c7580fc1a629a99c540b7a0396dad0a277df4
dkgiant <dangkynguyhiem@gmail.com>
2020-07-21 15:05:18 +0700

    add function Mysql:  - add category  - add author  - add publisher

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 import sys
 
 from PyQt5.uic import loadUiType
+import mysql.connector as connector
 
 ui, _ = loadUiType('library.ui')
 
@@ -26,6 +27,9 @@ class MainApp(QMainWindow, ui):
         self.btn_books.clicked.connect(self.Open_Books_Tab)
         self.btn_users.clicked.connect(self.Open_Users_Tab)
         self.btn_settings.clicked.connect(self.Open_Settings_Tab)
+        self.btn_add_category.clicked.connect(self.Add_Category)
+        self.btn_add_author.clicked.connect(self.Add_Author)
+        self.btn_add_publisher.clicked.connect(self.Add_Publisher)
 
     def Show_themes(self):
         self.gb_themes.show()
@@ -48,7 +52,15 @@ class MainApp(QMainWindow, ui):
 
     # -----------Books tab-------------------
     def Add_New_Book(self):
-        pass
+        self.db = connector.connect(host='localhost', user='root', password='Abcd@1234', db='library')
+
+        self.cur = self.db.cursor()
+        book_title = self.ltxt_add_book_title.text()
+        book_code = self.ltxt_add_book_code.text()
+        book_category = self.cb_book_category.CurrentText()
+        book_author = self.cb_book_author.CurrentText()
+        book_publisher = self.cb_book_publisher.CurrentText()
+        book_price = self.ltxt_add_book_price.text()
 
     def Search_Books(self):
         pass
@@ -68,6 +80,41 @@ class MainApp(QMainWindow, ui):
 
     def Login(self):
         pass
+
+    # -----------Settings tab-------------------
+    def Add_Category(self):
+        self.db = connector.connect(host='localhost', user='root', passwd='Abcd@1234', database='library')
+        self.cur = self.db.cursor()
+        category_name = self.ltxt_category_name.text()
+        self.cur.execute('''
+            INSERT INTO categories (category_name) VALUES (%s)
+        ''', (category_name,))
+
+        self.db.commit()
+        self.statusBar().showMessage('new category added')
+
+
+    def Add_Author(self):
+        self.db = connector.connect(host='localhost', user='root', passwd='Abcd@1234', database='library')
+        self.cur = self.db.cursor()
+        author_name = self.ltxt_author_name.text()
+        self.cur.execute('''
+            INSERT INTO authors (author_name) VALUES (%s)
+        ''', (author_name,))
+
+        self.db.commit()
+        self.statusBar().showMessage('new author added')
+
+    def Add_Publisher(self):
+        self.db = connector.connect(host='localhost', user='root', passwd='Abcd@1234', database='library')
+        self.cur = self.db.cursor()
+        publisher_name = self.ltxt_publisher_name.text()
+        self.cur.execute('''
+            INSERT INTO publishers (publisher_name) VALUES (%s)
+        ''', (publisher_name,))
+
+        self.db.commit()
+        self.statusBar().showMessage('new publisher added')
 
 
 def main():
